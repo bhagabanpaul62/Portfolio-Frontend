ffbb0d26dba61ee1944b9b8857520d7ffc6eb1d4
bhagabanpaul62 <160720125+bhagabanpaul62@users.noreply.github.com>
2025-05-24 14:31:12 +0530

    hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { SplashCursor } from "./components/background/SplashCursor";
+import { LandingHero } from "./components/homepage/landing-hero";
 
 import "./index.css";
 
 function App() {
   return (
-    <div className="min-h-screen w-full overflow-hidden relative bg-white text-black">
+    <div className="min-h-screen w-full overflow-hidden relative bg-white text-white">
       <SplashCursor>
-        
+        <LandingHero></LandingHero>
       </SplashCursor>
     </div>
   );
