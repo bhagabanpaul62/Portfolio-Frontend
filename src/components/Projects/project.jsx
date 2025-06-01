6eb60d6e708235e484d4d31ef9582ede1eb74ca7
bhagabanpaul62 <160720125+bhagabanpaul62@users.noreply.github.com>
2025-06-01 22:10:30 +0530

    fix vercel error

--- src/components/Projects/project.jsx
+++ src/components/Projects/project.jsx
@@ -1,20 +1,21 @@
-
 import { useRef } from "react";
-import { AllProject } from "./AllProjects";
-import { FeaturedProjects } from "./featuredProject";
+import { AllProject } from "./AllProjects.jsx";
+import { FeaturedProjects } from "./featuredProject.jsx";
 export const Project = () => {
-   const allProjectRef = useRef(null);
-    const scrollToAllProjects = () => {
-      
-      allProjectRef.current?.scrollIntoView({ behavior: "smooth" });
-    };
-return (
-  <div className="w-full min-h-screen px-4 py-20 lg:px-8">
-    {/* Header Section */}
-    <FeaturedProjects onMoreProjectClick={scrollToAllProjects} isHomePage={false}/>
+  const allProjectRef = useRef(null);
+  const scrollToAllProjects = () => {
+    allProjectRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+  return (
+    <div className="w-full min-h-screen px-4 py-20 lg:px-8">
+      {/* Header Section */}
+      <FeaturedProjects
+        onMoreProjectClick={scrollToAllProjects}
+        isHomePage={false}
+      />
 
-    {/* Search and Filter Section */}
-    <AllProject ref={allProjectRef} />
-  </div>
-);
-}
+      {/* Search and Filter Section */}
+      <AllProject ref={allProjectRef} />
+    </div>
+  );
+};
